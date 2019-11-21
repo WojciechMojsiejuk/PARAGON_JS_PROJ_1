51cfccf79ab0aa9729dd8a9bb853b886e94c1b14
GramBezEndu <33137304+GramBezEndu@users.noreply.github.com>
2019-11-21 17:26:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,11 +19,12 @@ class Receipt
 {
   constructor()
   {
-    
-    
+    this.records = new Array();
+    this.addRecord = function(name, quantity, price)
+    {
+      this.records.push(new Record(name, quantity, price));
+    }
   }
-
-  
 }
 
 class Record
@@ -34,27 +35,25 @@ class Record
     this.quantity = quantity;
     this.price = price;
   }
-
 }
-
 class Form
 {
-  constructor()
+  constructor(receipt)
   {
+    this.receipt = receipt;
     this.form_inputs = document.getElementsByTagName("input");
     this.record_obj_name = this.form_inputs[0];
     this.record_obj_quantity = this.form_inputs[1];
     this.record_obj_price = this.form_inputs[2];
     this.add_record_button = this.form_inputs[3];
+    this.addRecord = function()
+    {
+      this.receipt.addRecord(this.record_obj_name.value,
+        this.record_obj_quantity.value,
+        this.record_obj_price.value);
+    }
     this.add_record_button.onclick = this.addRecord.bind(this);
-
-  }
-  addRecord()
-  {
-    let name = this.record_obj_name.value;
-    let quantity = this.record_obj_quantity.value;
-    let price = this.record_obj_price.value;
-    let record = new Record(name,quantity,price);
   }
 }
-let receipt = new Receipt()
+let receipt = new Receipt();
+let Form = new Form(receipt);
